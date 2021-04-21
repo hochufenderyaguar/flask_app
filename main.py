2ff40709a24e30c693a11b813e952ae704f139e8
hochufenderyaguar <kabanova.sony@yandex.ru>
2021-04-21 20:40:06 +0300

    commit

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template
+from data import db_session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key'
 
 
-@app.route('/join') # регистрация
+@app.route('/join')  # регистрация
 def join():
     pass
 
-@app.route('/login') # вход
+
+@app.route('/login')  # вход
 def login():
     pass
 
-@app.route('/index') # страничка для пользователей
+
+@app.route('/index')  # страничка для пользователей
 def index():
     return render_template('index.html')
+
 
 @app.route('/base')
 def base():
@@ -21,4 +26,5 @@ def base():
 
 
 if __name__ == '__main__':
+    # db_session.global_init("db/monitor.db")
     app.run(port=8080, host='127.0.0.1')
